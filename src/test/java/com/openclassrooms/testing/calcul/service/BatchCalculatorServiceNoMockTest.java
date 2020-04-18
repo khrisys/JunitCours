e22688817d742ab45ebe8c77e37183ac7477558b
Christian Drochon (IJ) <khrisys@laposte.net>
2020-04-18 19:43:48 +0200

    Test des batchs du service CalculatorService avec tests de chacune des methodes utilisés dans CalculatorModel (add, sub, mult, divide). C'est plus precis que le "simple" test du CalculatorService qui repond bien dans le test precedent. Test que j'ai rebaptisé BatchCalculatorServiceNoMOckTest dans un autre classe pour bien differencier les 2 tests.
    Pour ca , on fait maintenant appel à la classe "ArgumentCaptor<>".
    Les tests à implenter commencent vraiment à devenir chauds!
    
    Import de assertJ, mais je sais pas encore comment bien faire. J'ecris encore la methode qui appele la classe static. J'arrive pas à declarer la dependance AssertJ static faisant partie du projet (et donc, je pourrais l'appeler sans pb)!
    
    Signed-off-by: Christian Drochon (IJ) <khrisys@laposte.net>

--- src/test/java/com/openclassrooms/testing/calcul/service/BatchCalculatorServiceNoMockTest.java
+++ src/test/java/com/openclassrooms/testing/calcul/service/BatchCalculatorServiceNoMockTest.java
@@ -2,14 +2,10 @@ package com.openclassrooms.testing.calcul.service;
 
 import com.openclassrooms.testing.calcul.domain.Calculator;
 import com.openclassrooms.testing.calcul.domain.model.CalculationModel;
-import com.openclassrooms.testing.calcul.domain.model.CalculationType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -18,26 +14,20 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
-import static org.mockito.Mockito.times;
 
-@ExtendWith(MockitoExtension.class)
-public class BatchCalculatorServiceTest {
+public class BatchCalculatorServiceNoMockTest {
     
     @Mock
     CalculatorService calculatorService;
     
     BatchCalculatorService batchCalculatorServiceNoMock;
     
-    BatchCalculatorService batchCalculatorService;
     
     @BeforeEach
     public void init() {
         
         batchCalculatorServiceNoMock = new BatchCalculatorServiceImpl(new CalculatorServiceImpl(new Calculator(),
                 new SolutionFormattedImpl()));
-        
-        batchCalculatorService = new BatchCalculatorServiceImpl(calculatorService);
     }
     
     @Test
@@ -53,26 +43,23 @@ public class BatchCalculatorServiceTest {
         
         
         //WHEN
-        //        final List<CalculationModel> resultsNoMock = batchCalculatorServiceNoMock.batchCalculate(operations);
-        final List<CalculationModel> results = batchCalculatorService.batchCalculate(operations);
+        final List<CalculationModel> resultsNoMock = batchCalculatorServiceNoMock.batchCalculate(operations);
         
         //THEN
-/*        assertThat(resultsNoMock).extracting(CalculationModel::getSolution) //extractiong d'assertj transforme la liste de
-                // results en
-                // liste de
-                // solutions, en appelant pour chaque item de la liste, la méthode getSolution()
+        assertThat(resultsNoMock).extracting(CalculationModel::getSolution) //extractiong d'assertj transforme la liste de
+                // results en liste de solutions, en appelant pour chaque item de la liste, la méthode getSolution()
                 .containsExactly(4, 1, 48, 3);
-        */
         // Lors de l'étape d'assertion, nous allons rapatrier tous les modèles de calcul utilisés. On en profite pour vérifier
         // que la classe  CalculatorService  a bien été utilisée autant de fois que de calculs dans le flux de données
-        Mockito
+/*        Mockito
                 .verify(calculatorService, times(4))
                 .calculate(vCalculationModelArgumentCaptor.capture());
-        final List<CalculationModel> calculationModels = vCalculationModelArgumentCaptor.getAllValues();
-        
+        final List<CalculationModel> calculationModels = vCalculationModelArgumentCaptor.getAllValues();*/
+/*
         assertThat(calculationModels)
                 .extracting(CalculationModel::getType, CalculationModel::getLeftArgument, CalculationModel::getRightArgument)
                 .containsExactly(tuple(CalculationType.ADDITION, 2, 2), tuple(CalculationType.SUBTRACTION, 5, 4),
-                        tuple(CalculationType.MULTIPLICATION, 6, 8), tuple(CalculationType.DIVISION, 9, 3));
+                        tuple(CalculationType.MULTIPLICATION, 6, 8), tuple(CalculationType.DIVISION, 9, 3));*/
     }
+    
 }
