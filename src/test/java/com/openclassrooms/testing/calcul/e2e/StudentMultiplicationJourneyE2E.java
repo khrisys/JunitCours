29419ab79888abc4053c5a2820b8602475a2ec7c
Christian Drochon (IJ) <khrisys@laposte.net>
2020-04-19 21:58:28 +0200

    Mise en place d'un test de bout en bout pour verifier une simple multiplcation. On indique le telechargement d'un driver d'un navigateur + on demande un port aleatoire qui ne soit aucun des ports utilisé sur la machine + on defini l'adresse du locaalhost.
    On cree les conditions du test.
    
    Grace aux methodes static de selenium, on peut assigner des ids dans le template calcultaor correspondant à la page html qu'on va afficher. On recupere ces ids dans la classe de test , ce qui correpond aux differents elements sur la page dont on a besoin pour faire les tests, y compris le bouton submit.
    
    Dans la partie WHEn, on defini des valeurs qu'uner aurait pu taper.
    Dans la partie THEN, on lance le navigateur, on stocke l'element de solution et on le compare avec la nb qu'on a ecrit en dur dans la classe de test.
    Apres le test, avec @AfterAll, on ferme le driver.
    
    Là, on a effectué" un test de bout en bout sur la mathode de multiplication du calculateur.
    
    Signed-off-by: Christian Drochon (IJ) <khrisys@laposte.net>

---
+++ src/test/java/com/openclassrooms/testing/calcul/e2e/StudentMultiplicationJourneyE2E.java
@@ -0,0 +1,114 @@
+package com.openclassrooms.testing.calcul.e2e;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * @ExtendWith(SpringExtension.class) permet de simuler une appli bien configurée, comme si on la mettait en prod
+ * @SpringBootTest utile pour les tests d'acceptation. Le param ajouté est utile pour dire qu'on lance l'appli comme un
+ * environnement de serveur web. Le port reseau est choisi au hasard, ce qui pemet d'eviter tout conflit avec un ou pluseirus
+ * autres serveurs web qui tourneraient sur la machine
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StudentMultiplicationJourneyE2E {
+    
+    // POrt reseau de connexion
+    @LocalServerPort
+    private Integer port;
+    private WebDriver webDriver = null; // environnement de test basé sur selenium
+    private String baseUrl;
+    
+    /**
+     * chaine de caractere de l'adresse d'accueil de l'appli web.
+     * Avec le webdriver, la 1ere chçose  àfaire avec le lancement de tous le stests est de s'assurer que ma machine possede
+     * bien un driver permettant de communiquer avec un navigateur. Car le test va ouvrir un VRAI  navigateur! Et il me faut ce
+     * logicile permettant d'effectuer cette communication
+     */
+    
+    @BeforeAll
+    public static void setUpFirefoxDriver() {
+        WebDriverManager
+                .firefoxdriver()
+                .setup();
+    }
+    
+    /**
+     * Avant le lancement de chaque test, initialisation du driver du navigateur. C'est l'etape qui fait ouvrir un VRAI
+     * navigateur web
+     */
+    @BeforeEach
+    public void setUpWebDriver() {
+        webDriver = new FirefoxDriver();
+        baseUrl = "http://localhost:" + port + "/calculator";
+        
+    }
+    
+    /**
+     * Fermeture du navigateur à chaque fin de test
+     */
+    @AfterEach
+    public void quitWebDriver() {
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+    
+    /**
+     * Dans l'etape GIVEN, on demande au navigateur d'aller directement sur l'url de base du calculateur. On recupere alors sur
+     * la page obtenue les elements web obtenus correspondant aux champs saisis des operande et du typoe d'operation + au
+     * bouton submit du form
+     *
+     * Etape GIVEN
+     * On recupere ces elements  grace à la methode static de selenium "By.id(...)". Il s'agit des ids Html. Il faut pour que
+     * ca fonctionne aller dans la page de template pour ajouter correctement les ids. (template dans
+     * resources/templates/calculator.html
+     *
+     * Etape WHEN
+     * On remplit les elements comme si un user remplissait les champs au clavier (methode 'sendKeys()') et on simule le submit
+     * (avec la methode 'click()').
+     *
+     * Etape THEN
+     * On crée un objet qui va attendre que la page de retour se recharge. Pour ca, on indique au webDriver qui'l y ait sur la
+     * page un element d'id 'solution'. Puis , on extrait le contenu de l'element 'solution' et on verifie avec une assertion que
+     * cet element contient bien le nb 'solution' de la multiplication
+     */
+    @Test
+    public void aStudentUsesTheCalculatorToMultiplyTwoBySixteen() {
+        // GIVEN
+        webDriver.get(baseUrl);
+        final WebElement leftField = webDriver.findElement(By.id("left"));
+        final WebElement typeDropdown = webDriver.findElement(By.id("type"));
+        final WebElement rightField = webDriver.findElement(By.id("right"));
+        final WebElement submitButton = webDriver.findElement(By.id("submit"));
+        
+        // WHEN
+        leftField.sendKeys("2");
+        typeDropdown.sendKeys("x");
+        rightField.sendKeys("16");
+        submitButton.click();
+        
+        // THEN
+        final WebDriverWait waiter = new WebDriverWait(webDriver, 5);
+        // Stockage de l'element 'solution' dans une var
+        final WebElement solutionElement = waiter.until(ExpectedConditions.presenceOfElementLocated(By.id("solution")));
+        final String solution = solutionElement.getText();
+        assertThat(solution).isEqualTo("32"); // 2 x 16
+    }
+    
+}
